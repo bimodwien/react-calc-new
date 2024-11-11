6ae6ad955330d4e46899b5e382f3b2daf853b790
bimodwien <bimodwien2@gmail.com>
2024-11-11 11:55:43 +0700

    update: layouting pt.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Result from "./components/Result";
+import Action from "./components/Action";
+import History from "./components/History";
 
 function App() {
   return (
     <>
-      <div className="text-3xl underline font-bold">Hello World</div>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="block px-10 border border-black  w-2/6 h-[750px]">
+          <Result />
+          <Action />
+        </div>
+        <div className="border border-black w-1/6 h-[750px]">
+          <History />
+        </div>
+      </div>
     </>
   );
 }
